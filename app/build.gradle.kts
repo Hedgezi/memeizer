e879daff290a07ce93b5b2533ef25870176b4d53
Mykola V <nikola@rootnix.net>
2026-06-05 18:16:10 +0200

    update libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,21 +34,21 @@ kotlin {
 }
 
 dependencies {
-    val roomVersion = "2.6.1"
-    val lifecycleVersion = "2.8.7"
+    val roomVersion = "2.8.4"
+    val lifecycleVersion = "2.10.0"
 
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2026.05.01"))
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.documentfile:documentfile:1.0.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
-    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.navigation:navigation-compose:2.8.9")
     implementation("androidx.room:room-ktx:$roomVersion")
     implementation("androidx.room:room-runtime:$roomVersion")
-    implementation("androidx.work:work-runtime-ktx:2.10.0")
+    implementation("androidx.work:work-runtime-ktx:2.11.2")
     implementation("com.google.mlkit:text-recognition:16.0.1")
     implementation(project(":ncnnAndroidPPOCR"))
     implementation("io.coil-kt:coil-compose:2.7.0")
